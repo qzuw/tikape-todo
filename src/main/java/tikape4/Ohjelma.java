706cb141121bc769acdcf6ffdeb1aebc35e12387
avihavai <avihavai@x1c-9.cs.helsinki.fi>
2016-10-04 12:54:26 +0300

    ok

--- src/main/java/tikape4/Ohjelma.java
+++ src/main/java/tikape4/Ohjelma.java
@@ -5,11 +5,35 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import spark.Spark;
+import static spark.Spark.port;
 
 public class Ohjelma {
 
     public static void main(String[] args) throws Exception {
-        Spark.get("*", (req, res) -> "hello");
+        port(getHerokuAssignedPort());
+
+
+        Spark.get("*", (req, res) -> {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:todo.db");
+            Statement stmt = conn.createStatement();
+            ResultSet result = stmt.executeQuery("SELECT * FROM Todo");
+            
+            String vastaus = "";
+            while (result.next()) {
+                vastaus += result.getString("task") + "<br/>";
+            }
+            
+            conn.close();
+
+            return vastaus;
+        });
     }
 
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
